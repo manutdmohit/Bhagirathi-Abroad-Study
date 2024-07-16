93e76cff6c5e9e3abf066a72a9450adff96d38b8
Mohit Saud <mohit@prabidhilabs.com>
2024-07-16 12:35:09 +0545

    Update Footer alignment for small devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,27 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white py-8 mt-8">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-around items-center md:items-start text-center md:text-left space-y-8 md:space-y-0">
-          <div>
+        <div className="flex flex-col md:flex-row justify-between items-start text-left space-y-8 md:space-y-0">
+          <div className="flex flex-col items-start md:items-start">
             <h2 className="text-white text-2xl font-bold">
               Bhagirathi Abroad Study Pvt. Ltd
             </h2>
             <p className="text-white mt-2">Guiding Your Success, Together</p>
           </div>
-          <div className="flex flex-col md:flex-row md:space-x-16">
-            <div className="space-y-4">
+          <div className="flex flex-col md:flex-row md:space-x-16 w-full md:w-auto">
+            <div className="space-y-4 text-left">
               <h3 className="text-white text-xl font-semibold">Contact Us</h3>
-              <p className="text-white">Phone: +977 980 1711850</p>
-              <p className="text-white">Email: info@bhagirathi.com</p>
+              <Link href="tel:+9779801711850">
+                <p className="text-white mt-4">Phone: +977 980 1711850</p>
+              </Link>
+              <Link href="mailto:baspl202477@gmail.com">
+                <p className="text-white mt-2">Email: baspl202477@gmail.com</p>
+              </Link>
               <p className="text-white">
                 Address: Lamki Tikapur Road, Tikapur-1, Kailali
               </p>
             </div>
-            <div className="space-y-4 mt-4 md:mt-0">
+            <div className="space-y-4 mt-4 md:mt-0 text-left">
               <h3 className="text-white text-xl font-semibold">Quick Links</h3>
               <ul className="space-y-2">
                 <li>
